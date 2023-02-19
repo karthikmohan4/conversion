299004a8b0ea1602649cab57fb8fd24448a45473
Karthik M <mkarthik556@gmail.com>
2023-02-19 14:24:38 +0530

    binary,hex,radians,octal,degree

--- conversion/lib/conversion.dart
+++ conversion/lib/conversion.dart
@@ -1,15 +1,66 @@
 library conversion;
 
+export 'src/core/conversion_methods.dart';
 import 'package:conversion/src/core/conversion_methods.dart';
 
 class Convert {
   Conversion conversion = Conversion();
 
-  decimalToHex({required List<int> values}) {
-    print(conversion.convertDecimalToHex(values));
+//decimal to hex
+  List<String> decimalToHex({required List<int>? values}) {
+    return conversion.convertDecimalToHex(decimalValues: values!);
   }
 
-  decimalToBinary({required List<int> values}) {
-    print(conversion.convertDecimalToBinary(values));
+//decimal to binary
+  List<String> decimalToBinary({required List<int>? values}) {
+    return conversion.convertDecimalToBinary(decimalValues: values!);
+  }
+
+//binary to decimal
+  List<int> binaryTodecimal({required List<String>? values}) {
+    return conversion.convertBinaryToDecimal(binaryValues: values!);
+  }
+
+  //binary to hex
+  List<String> binaryToHex({required List<String>? values}) {
+    return conversion.convertBinaryToHex(binaryValues: values!);
+  }
+
+  //hex to binary
+  List<String> hexToBinary({required List<String>? values}) {
+    return conversion.convertHexToBinary(hexValues: values!);
+  }
+
+  //hexadecimal to decimal
+  List<int> hexToDecimal({required List<String>? hexString}) {
+    return conversion.convertHexToDecimal(hexString: hexString!);
+  }
+
+  List<int> decimalToOctal({required List<int>? values}) {
+    return conversion.convertDecimalToOctal(decimalValues: values!);
+  }
+
+//degrees, minute, second to decimal degree
+  double toDecimalDegree(
+      {required double? degree,
+      required double? minutes,
+      required double? seconds}) {
+    return conversion.convertToDecimalDegree(
+        degree: degree!, minutes: minutes!, seconds: seconds!);
+  }
+
+//radians to degree
+  double radiansToDegree({required double? radians}) {
+    return conversion.radiansToDegree(radians: radians!);
+  }
+
+  //find one's compliment
+  String findOnesComplement({required String? binary}) {
+    return conversion.findOnesComplement(binary: binary!);
+  }
+
+//find two's complement
+  String findTwosComplement({required String? binary}) {
+    return conversion.findTwosComplement(binary: binary!);
   }
 }
--- conversion/lib/src/core/conversion_methods.dart
+++ conversion/lib/src/core/conversion_methods.dart
@@ -1,13 +1,70 @@
+import 'dart:math';
+
 class Conversion {
   //Decimal to hexadecimal
-  Iterable<String> convertDecimalToHex(List<int> decimalValues) {
-  return decimalValues.map((e) => e.toRadixString(16).toString());
+  List<String> convertDecimalToHex({required List<int> decimalValues}) {
+    return decimalValues.map((e) => e.toRadixString(16).toString()).toList();
   }
 
-  //Decimal to binary 
-  Iterable<String> convertDecimalToBinary(List<int> decimalValues) {
-  return decimalValues.map((e) => e.toRadixString(2).toString());
+  //Decimal to binary
+  List<String> convertDecimalToBinary({required List<int> decimalValues}) {
+    return decimalValues.map((e) => e.toRadixString(2).toString()).toList();
   }
+
+  //binary to decimal
+  List<int> convertBinaryToDecimal({required List<String> binaryValues}) {
+    return binaryValues.map((e) => int.parse(e, radix: 2)).toList();
+  }
+
+  //binary to decimal
+  List<String> convertBinaryToHex({required List<String> binaryValues}) {
+    List<int> decimal = convertBinaryToDecimal(binaryValues: binaryValues);
+    return convertDecimalToHex(decimalValues: decimal);
+  }
+
+  //hex to binary
+  List<String> convertHexToBinary({required List<String> hexValues}) {
+    List<int> decimal = convertHexToDecimal(hexString: hexValues);
+    return convertDecimalToBinary(decimalValues: decimal);
+  }
+
+  //decimal to octal
+  List<int> convertDecimalToOctal({required List<int> decimalValues}) {
+    return decimalValues.map((e) => int.parse(e.toRadixString(8))).toList();
+  }
+
+  //degree, minutes, seconds to degree conversion
+  double convertToDecimalDegree(
+      {required double degree,
+      required double minutes,
+      required double seconds}) {
+    return (degree) + (minutes / 60) + (seconds / 3600);
+  }
+
+//radians to degree
+  double radiansToDegree({required double radians}) {
+    return (radians) * (180 / pi);
+  }
+
+  //hexadecimal to decimal
+  List<int> convertHexToDecimal({required List<String> hexString}) {
+    return hexString.map((e) => int.parse(e, radix: 16)).toList();
+  }
+
  
 
+  //find one's compliment
+  String findOnesComplement({required String binary}) {
+    return binary
+        .toString()
+        .split('')
+        .map((bit) => bit == '0' ? '1' : '0')
+        .join();
+  }
+
+  //find two's complement
+  String findTwosComplement({required String binary}) {
+    final oneComplement = findOnesComplement(binary: binary);
+    return (int.parse(oneComplement, radix: 2) + 1).toRadixString(2);
+  }
 }
